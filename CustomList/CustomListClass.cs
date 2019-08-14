0fb8077d9118a3fc17caff4f720113ad70b1c27e
Jon Ransom <jransom12@live.com>
2019-08-14 13:50:49 -0500

    Added Count and Capacity properties and Indexer

--- CustomList/CustomListClass.cs
+++ CustomList/CustomListClass.cs
@@ -8,10 +8,63 @@ namespace CustomList
 {
     public class CustomListClass<T>
     {
+        // member variables
         private T[] items;
+        private List<T> customList;
+        private int count;
+        public int capacity;
+
+        // constructor
+        public CustomListClass()
+        {
+            customList = new List<T>();
+            items = new T[4];
+        }
+
+        // member methods
         public void Add(T itemToAdd)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            //set
+            //{
+            //    capacity = value;
+            //}
+        }
+        public T this[int index] //Indexer
+        {
+            get
+            {
+                if (index < 0 && index >= items.Length)
+                    throw new IndexOutOfRangeException("Cannot store more than " + items.Length + " objects");
+
+                return items[index];
+            }
+            set
+            {
+                if (index < 0 && index >= items.Length)
+                    throw new IndexOutOfRangeException("Cannot store more than " + items.Length + " objects");
+
+                items[index] = value;
+            }
         }
     }
 }
--- CustomListTest/CustomListClassTests.cs
+++ CustomListTest/CustomListClassTests.cs
@@ -11,14 +11,14 @@ namespace CustomListTest
         public void Add_AddToListWithExistingItem_CounterGoesUpOne()
         {
             // arrange
-            CustomListClass<int> testList = new CustomListClass<int>();
+            CustomListClass<int> customList = new CustomListClass<int>();
             int expected = 2;
             int actual;
 
             // act
-            testList.Add(1);
-            testList.Add(2);
-            actual = testList.Count;
+            customList.Add(1);
+            customList.Add(2);
+            actual = customList.Count;
 
             // assert
             Assert.AreEqual(expected, actual);
@@ -29,17 +29,17 @@ namespace CustomListTest
         public void Add_AddToListAtMaxCapacity_ArraySizeDoubles()
         {
             // arrange
-            CustomListClass<int> testList = new CustomListClass<int>();
+            CustomListClass<int> customList = new CustomListClass<int>();
             int expected = 5;
             int actual;
 
             // act
-            testList.Add(1);
-            testList.Add(2);
-            testList.Add(3);
-            testList.Add(4);
-            testList.Add(5);
-            actual = testList.Count;
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+            customList.Add(5);
+            actual = customList.Count;
 
             // assert
             Assert.AreEqual(expected, actual);
@@ -48,21 +48,21 @@ namespace CustomListTest
         public void Add_AddToListAtMaxCapacity_ArraySizeDoublesToSixteen()
         {
             // arrange
-            CustomListClass<int> testList = new CustomListClass<int>();
+            CustomListClass<int> customList = new CustomListClass<int>();
             int expected = 9;
             int actual;
 
             // act
-            testList.Add(1);
-            testList.Add(2);
-            testList.Add(3);
-            testList.Add(4);
-            testList.Add(5);
-            testList.Add(6);
-            testList.Add(7);
-            testList.Add(8);
-            testList.Add(9);
-            actual = testList.Count;
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+            customList.Add(5);
+            customList.Add(6);
+            customList.Add(7);
+            customList.Add(8);
+            customList.Add(9);
+            actual = customList.Count;
 
             // assert
             Assert.AreEqual(expected, actual);
@@ -71,13 +71,13 @@ namespace CustomListTest
         public void Add_AddToEmptyList_ItemGoesToIndexZero()
         {
             // arrange
-            CustomListClass<int> testList = new CustomListClass<int>();
+            CustomListClass<int> customList = new CustomListClass<int>();
             int expected = 1;
             int actual;
 
             // act
-            testList.Add(1);
-            actual = testList[0];
+            customList.Add(1);
+            actual = customList[0];
 
             // assert
             Assert.AreEqual(expected, actual);
@@ -87,14 +87,14 @@ namespace CustomListTest
         public void Add_AddToListWithExistingItem_NewItemGoesToEndOfList()
         {
             // arrange
-            CustomListClass<int> testList = new CustomListClass<int>();
+            CustomListClass<int> customList = new CustomListClass<int>();
             int expected = 2;
             int actual;
 
             // act
-            testList.Add(1);
-            testList.Add(2);
-            actual = testList[1];
+            customList.Add(1);
+            customList.Add(2);
+            actual = customList[1];
 
             // assert
             Assert.AreEqual(expected, actual);
@@ -104,13 +104,13 @@ namespace CustomListTest
         public void Add_AddToEmptyList_CountGoesToOne()
         {
             // arrange
-            CustomListClass<int> testList = new CustomListClass<int>();
+            CustomListClass<int> customList = new CustomListClass<int>();
             int expected = 1;
             int actual;
 
             // act
-            testList.Add(198765);
-            actual = testList.Count;
+            customList.Add(198765);
+            actual = customList.Count;
 
             // assert
             Assert.AreEqual(expected, actual);
